917517319e7a5a19db4626a97c6d8963a9cb3b03
Andrei V. Berezhkov <berezhkov.mail@yandex.ru>
2024-09-04 16:36:02 +0300

    add client

---
+++ client/src/app/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import UserCount from "../components/UserCount/UserCount";
+
+function App() {
+  return (
+    <>
+      <UserCount />
+    </>
+  );
+}
+
+export default App;
