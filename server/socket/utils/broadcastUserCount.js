4248cd50333352f3c6c6dbd0dc05182d6888142d
Andrei V. Berezhkov <berezhkov.mail@yandex.ru>
2024-09-04 16:39:11 +0300

    fix

--- server/socket/utils/broadcastUserCount.js
+++ server/socket/utils/broadcastUserCount.js
@@ -1,13 +1,7 @@
 function broadcastUserCount(wss) {
     const userCount = wss.clients.size;
     const message = JSON.stringify({ type: 'USER_COUNT', data: userCount });
-    console.log(message);
-    
-    for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      
-    }
-  
+
     wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
